d78c52ebec357143b32bdda7914e60dbbccc2eec
Anthony Ruffin <aruffincs@gmail.com>
2021-10-20 10:59:24 -0400

    added styles to button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button" type="submit">Increase Count</button>
+</template>
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+<style lang="scss">
+@import '../assets/global-styles';
+
+.button {
+  padding: 10px 20px;
+  border: solid 1px $white;
+  border-radius: 4px;
+  background-color: $dark-violet;
+  color: $white;
+}
+</style>
