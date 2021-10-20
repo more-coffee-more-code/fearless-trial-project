1243d8b57bd237c80d74cf67a5030ac5142c7df8
Anthony Ruffin <aruffincs@gmail.com>
2021-10-20 10:21:07 -0400

    removed default vue-cli files and components

---
+++ src/components/NumberDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="display">
+    <div class="display__wrapper">
+      <p class="display__number"></p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NumberDisplay'
+}
+</script>
+<style lang="scss">
+.display {
+  &__wrapper {
+
+  }
+
+  &__number {
+
+  }
+}
+</style>
