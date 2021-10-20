b2b0815ede0a2011af66f406dde9c72d8b397b1f
Anthony Ruffin <aruffincs@gmail.com>
2021-10-20 12:07:49 -0400

    added props to allow parent to update number displays

--- src/components/NumberDisplay.vue
+++ src/components/NumberDisplay.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="display">
     <div class="display__wrapper">
-      <p class="display__number"></p>
+      <p class="number-display" v-if="displayCount === true">{{ currentCount }}</p>
+      <p class="number-display" v-if="displayHits === true">{{ displayHits }}</p>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'NumberDisplay'
+  name: 'NumberDisplay',
+  props: {
+    currentCount: Number,
+    totalHits: Number,
+    displayCount: Boolean,
+    displayHits: Boolean
+  },
+  methods: {
+  
+  }
 }
 </script>
 <style lang="scss">
